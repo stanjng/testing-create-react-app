ae6d210d2c2f57b5221339a84443bb5c91bf558b
stanjng <sljadverts@gmail.com>
2020-05-05 14:08:53 -0400

    Updates

---
+++ precache-manifest.9dc64b2319fd8dbd52e0cdcb524dfe7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4ec1edcfc1a7c55842400d680d0c0f6",
+    "url": "/testing-create-react-app/index.html"
+  },
+  {
+    "revision": "083ae63f9da116394a17",
+    "url": "/testing-create-react-app/static/css/main.a689ad72.chunk.css"
+  },
+  {
+    "revision": "5b89ec902466d85ac0f5",
+    "url": "/testing-create-react-app/static/js/2.7501cc90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testing-create-react-app/static/js/2.7501cc90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "083ae63f9da116394a17",
+    "url": "/testing-create-react-app/static/js/main.1f502e1f.chunk.js"
+  },
+  {
+    "revision": "9069a2ed7a512d8d3860",
+    "url": "/testing-create-react-app/static/js/runtime-main.f6e45677.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testing-create-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
